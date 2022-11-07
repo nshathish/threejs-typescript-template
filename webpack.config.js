bf5355cf5d74371445efff3492d2758a13fe3ff8
Adolphous Shathish <nshathish@hotmail.com>
2022-11-07 07:42:26 +0000

    2-using webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,21 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  mode: 'none',
+  mode: 'development',
   entry: './src/index.ts',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
   },
   resolve: {
     alias: {
@@ -13,12 +23,15 @@ module.exports = {
     },
     extensions: ['.ts', '.js']
   },
-  module: {
-    rules: [
-      {
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
-  }
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      template: './src/index.html',
+    }),
+  ],
 };
